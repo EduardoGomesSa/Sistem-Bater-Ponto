233dc0b7ff5729a4feb46a2a8c7f7dfc74096c9a
EduardoGomesSa <eduardo.sa.022@gmail.com>
2022-09-04 22:22:27 -0300

    Estrutura do Sistema de bater ponto

---
+++ Arquivos-SistemaBaterPonto/BaterPonto.Infra/Entities/Cargo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaterPonto.Infra.Entities
+{
+    public class Cargo
+    {
+        public Cargo(int id, string? nome, int cargaHoraria)
+        {
+            Id = id;
+            Nome = nome;
+            CargaHoraria = cargaHoraria;
+        }
+
+        public int Id { get; private set; }
+        public string? Nome { get; private set; }
+        public int CargaHoraria { get; private set; }
+    }
+}
---
+++ Arquivos-SistemaBaterPonto/BaterPonto.Infra/Entities/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaterPonto.Infra.Entities
+{
+    public class Funcionario
+    {
+        public Funcionario(int id, string cpf, decimal salario, int horasTrabalhadas)
+        {
+            Id = id;
+            Cpf = cpf;
+            Salario = salario;
+            HorasTrabalhadas = horasTrabalhadas;
+        }
+
+        public int Id { get; private set; }
+        public string Cpf { get; set; }
+        public decimal Salario { get; private set; }
+        public int HorasTrabalhadas { get; private set; }
+    }
+}
