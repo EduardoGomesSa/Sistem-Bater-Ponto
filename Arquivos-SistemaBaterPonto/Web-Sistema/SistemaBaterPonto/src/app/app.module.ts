677547d82168dae3f7d42de613d44d51d2582b29
EduardoGomesSa <eduardo.sa.022@gmail.com>
2022-10-20 00:26:04 -0300

    Limpeza do código html padrão e criação dos componentes de navegação

--- Arquivos-SistemaBaterPonto/Web-Sistema/SistemaBaterPonto/src/app/app.module.ts
+++ Arquivos-SistemaBaterPonto/Web-Sistema/SistemaBaterPonto/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
