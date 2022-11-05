b0928c93f4099dbda4f74e9d9effb4d6b4becae4
Eduardo Gomes <eduardo.sa.022@gmail.com>
2022-11-05 00:37:15 -0300

    Criação da classe de testes dos services

---
+++ Arquivos-SistemaBaterPonto/Api-Sistema/BaterPonto.Testes/Services/CadastroFuncionarioServiceTestes.cs
@@ -0,0 +1,15 @@
+﻿
+using Xunit;
+
+namespace BaterPonto.Testes.Services
+{
+    public class CadastroFuncionarioServiceTestes
+    {
+        [Fact(DisplayName = "Adicionar Sucesso")]
+        [Trait("Categoria", "Service Funcionario")]
+        public void Services_Adicionar_Sucesso()
+        {
+
+        }
+    }
+}
